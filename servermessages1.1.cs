02170940899a33ccdd456381c8e2f48a43163a08
NicoTamborello <31487356+NicoTamborello@users.noreply.github.com>
2025-04-17 01:20:50 -0400

    Update servermessages1.1.cs

--- servermessages1.1.cs
+++ servermessages1.1.cs
@@ -26,6 +26,48 @@ namespace ScheduledMessages
         EnumChatType.Chat
     ),
     (
+        "§fThis is the age of the Seraph...§f " +
+        "The world is indeed comic, but the joke is on mankind. " +
+        "§ - H. P. Lovecraft§6",
+        EnumChatType.Chat
+    ),
+    (
+        "§fThis is the age of the Seraph...§f " +
+        "Prosperity knits a man to the world " +
+        "He feels that he is finding his place in it, " +
+        "while really it is finding its place in him. " +
+        "§6- C.S. Lewis§6",
+        EnumChatType.Chat
+    ),
+    (
+        "§fThis is the age of the Seraph...§f " +
+        "I cannot forget Carcosa where black stars hang in the heavens; where " +
+        "the shadows of men's thoughts lengthen in the afternoon, when the " +
+        "twin suns sink into the lake of Hali; and my mind will bear for ever the " +
+        "memory of the Pallid Mask. I pray God will curse the writer, as the writer " +
+        "has cursed the world with this beautiful, stupendous creation, terrible in " +
+        "its simplicity, irresistible in its truth—a world which now trembles " +
+        "before the King in Yellow. " +
+        "while really it is finding its place in him. " +
+        "§6- Robert W. Chambers, The King in Yellow§6",
+        EnumChatType.Chat
+    ),
+    (
+       "§fThis is the age of the Seraph...§f " +
+        "And strange moons circle through the skies " +
+        "But Stranger still is " +
+        "Lost Carcosa. " +
+        "§6- Robert W. Chambers, The King in Yellow§6",
+        EnumChatType.Chat
+    )
+    (
+        "§fWords§f " +
+        "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. " +
+        "In his house at R'lyeh dead Cthulhu waits dreaming " +
+        "§6- H.P. Lovecraft, The Call of Cthulhu§6",
+        EnumChatType.Chat
+    ),
+    (
         "§fWords§f " +
         "Words " +
         "Words " +
@@ -33,29 +75,75 @@ namespace ScheduledMessages
         EnumChatType.Chat
     ),
     (
-        "§fThis is the age of the Seraph...§f " +
-        "Prosperity knits a man to the world " +
-        "He feels that he is finding his place in it, " +
-        "while really it is finding its place in him. " +
-        "§6- C.S. Lewis§6",
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
         EnumChatType.Chat
     ),
     (
-        "§fThis is the age of the Seraph...§f " +
-        "Prosperity knits a man to the world " +
-        "He feels that he is finding his place in it, " +
-        "while really it is finding its place in him. " +
-        "§6- C.S. Lewis§6",
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
         EnumChatType.Chat
     ),
     (
-       "§fThis is the age of the Seraph...§f " +
-        "Prosperity knits a man to the world " +
-        "He feels that he is finding his place in it, " +
-        "while really it is finding its place in him. " +
-        "§6- C.S. Lewis§6",
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
         EnumChatType.Chat
-    )    
+    ),
+    (
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
+        EnumChatType.Chat
+    ),
+    (
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
+        EnumChatType.Chat
+    ),
+    (
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
+        EnumChatType.Chat
+    ),
+    (
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
+        EnumChatType.Chat
+    ),
+    (
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
+        EnumChatType.Chat
+    ),
+    (
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
+        EnumChatType.Chat
+    ),
+    (
+        "§fWords§f " +
+        "Words " +
+        "Words " +
+        "§6- Author§6",
+        EnumChatType.Chat
+    ),    
         };
 
         public override void StartServerSide(ICoreServerAPI api)
