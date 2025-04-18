c3f4c1f4c56ec85566d6bd566c3738f6cfdeeac6
NicoTamborello <31487356+NicoTamborello@users.noreply.github.com>
2025-04-18 00:48:49 -0400

    Renaming to 1.02
    New class descriptions have been added and require README to be updated with them as I was using an old set of classes

--- scheduledmessages1.01.cs
+++ scheduledmessages1.01.cs
@@ -10,7 +10,7 @@ namespace ScheduledMessages
         private double lastMessageTime = 0;
         private double messageInterval = 60; // was 600 for 10 minutes in seconds - set to 60 seconds now
         private int messageIndex = 0;
-        
+
         // Each message has content and a message type
         // §c = red §a = green §e = yellow §b = aqua §6 = gold §f = white
         private (string message, EnumChatType type)[] messages = new (string, EnumChatType)[]
@@ -19,17 +19,17 @@ namespace ScheduledMessages
         "§fThis is the age of the Seraph...§f " +
         "Lifes but a walking shadow, a poor player " +
         "That struts and frets his hour upon the stage, " +
-        "And then is heard no more. It is a tale"
+        "And then is heard no more. It is a tale " +
         "Told by an idiot, full of sound and fury, " +
         "Signifying nothing. " +
         "§6- Macbeth§6",
-        EnumChatType.Server
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "The world is indeed comic, but the joke is on mankind. " +
         "§ - H. P. Lovecraft§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
@@ -37,7 +37,7 @@ namespace ScheduledMessages
         "He feels that he is finding his place in it, " +
         "while really it is finding its place in him. " +
         "§6- C.S. Lewis§6",
-        EnumChatType.Server
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
@@ -50,54 +50,54 @@ namespace ScheduledMessages
         "before the King in Yellow. " +
         "while really it is finding its place in him. " +
         "§6- Robert W. Chambers, The King in Yellow§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
        "§fThis is the age of the Seraph...§f " +
         "And strange moons circle through the skies " +
         "But Stranger still is Lost Carcosa." +
         "§6- Robert W. Chambers, The King in Yellow§6",
-        EnumChatType.Chat
-    )
+        EnumChatType.AllGroups
+    ),
     (
         "§fThis is the age of the Seraph...§f " +
         "Ph'nglui mglw'nafh Cthulhu R'lyeh wgah'nagl fhtagn. " +
         "In his house at R'lyeh dead Cthulhu waits dreaming " +
         "§6- H.P. Lovecraft, The Call of Cthulhu§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "A man can endure anything, as long as he knows the end will come. " +
         "§6- Haruki Murakami§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "If you're going through hell, keep going." +
         "§6- Winston Churchill§6",
-        EnumChatType.Chat
-    ),
+        EnumChatType.AllGroups
+    ),      
     (
         "§fThis is the age of the Seraph...§f " +
         "He who is not bold enough to be stared at  " +
         "from across the abyss is not bold enough " +
         "to stare into it himself " +
         "§6- Memo, Silent Hill 2§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "The fear of blood tends to create fear for the flesh. " +
         "§6- Silent Hill Opening§6",
-        EnumChatType.Server
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "Notice that the stiffest tree is most easily cracked, while " +
         "the bamboo or willow survives by bending with the wind. " +
         "§6- Bruce Lee§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
@@ -105,47 +105,47 @@ namespace ScheduledMessages
         "the spaces we know, but between them. They walk serene and primal, " +
         "undimensioned and to us unseen. " +
         "§6- H.P. Lovecraft§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "It is only by way of pain one arrives at pleasure " +
         "§6- Marquis de Sade§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "Horror is the badge of humanity, worn proudly, self-righteously, and " +
         "often falsely " +
         "§6- Poppy Z. Brite, Exquisite Corpse§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "It is sometimes an appropriate response to reality to go insane. " +
         "§6- Philip K. Dick, VALIS§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "We are not enemies, but friends. We must not be enemies. " +
         "Though passion may have strained it must not break our bonds of affection. " +
-        "The mystic chords of memory, will yet swell when again touched, as surely " + 
+        "The mystic chords of memory, will yet swell when again touched, as surely " +
         "they will be, by the better angels of our nature " +
         "§6- Abraham Lincoln§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "He who has a why to live can bear almost any how. " +
         "§6- Friedrich Nietzsche§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
         "Extinction is the rule. Survival is the exception. " +
         "§6- Carl Sagan§6",
-        EnumChatType.Chat
+        EnumChatType.AllGroups
     ),
     (
         "§fThis is the age of the Seraph...§f " +
@@ -155,8 +155,8 @@ namespace ScheduledMessages
         "would stand yet, drinking in the last light of the dying sun, even as now, " +
         "when I had been dead as long as those who rested there. " +
         "§6- Gene Wolfe, The Book of the New Sun§6",
-        EnumChatType.Chat
-    ),    
+        EnumChatType.AllGroups  
+    ),
         };
 
         public override void StartServerSide(ICoreServerAPI api)
@@ -181,3 +181,4 @@ namespace ScheduledMessages
         }
     }
 }
+
